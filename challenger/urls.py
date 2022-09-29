673a8b6f0a9f6da198db9988c576fd3b20bf73fe
Roman <romannkonyahin@gmail.com>
2022-09-29 17:23:22 +0300

    Добавлен вывод списка челленджей на главной странице,  страница создания челленджа.

--- challenger/urls.py
+++ challenger/urls.py
@@ -1,26 +1,14 @@
-"""challenger URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
-from challenges.views import ChallengeAPIView, ChallengeItemAPIView
+from challenges.views import ChallengeAPIView, ChallengeItemAPIView, index, create_challenge, new_challenge
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('challenge/', ChallengeAPIView.as_view()),
     path('challenge/<pk>', ChallengeItemAPIView.as_view()),
+    path("", index),
+    path("create_challenge", create_challenge),
+    path("new_challenge", new_challenge),
 
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from django.shortcuts import render
 from challenges.models import Challenge
 from challenges.serializers import ChallengeSerializer, ChallengePostSerializer
+from django.shortcuts import redirect
 
 
 class ChallengeAPIView(APIView):
@@ -12,12 +13,14 @@ class ChallengeAPIView(APIView):
     serializer_class = ChallengeSerializer
 
     def get(self, request):
+        """Вывод списка всех челленджей"""
         challenges = Challenge.objects.all()
         serializer = self.serializer_class(challenges, many=True)
         return Response(serializer.data)
 
     @staticmethod
     def post(request):
+        """Публикация челленджа"""
         serializer = ChallengePostSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
@@ -27,20 +30,58 @@ class ChallengeAPIView(APIView):
 class ChallengeItemAPIView(APIView):
     @staticmethod
     def get(request, pk):
+        """Получить челлендж по id"""
         challenge = Challenge.objects.get(id=pk)
         serializer = ChallengeSerializer(challenge, many=False)
-        return Response(serializer.data)
+        context = {'title': challenge.title, 'reward': challenge.reward, 'date': challenge.date, 'description':challenge.description }
+        # return Response(serializer.data)
+        return render(request, "challenge.html", context)
 
     @staticmethod
     def delete(request, pk):
+        """Удалить челлендж"""
         challenge = Challenge.objects.get(id=pk)
         challenge.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     @staticmethod
     def put(request, pk):
+        """Редактировать челлендж"""
         challenge = Challenge.objects.get(id=pk)
         serializer = ChallengeSerializer(challenge, data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+def index(request):
+    challenges = Challenge.objects.all()
+    output = list()
+    for challenge in challenges:
+        res = dict()
+        res['id'] = challenge.id
+        res['title'] =challenge.title
+        res['description'] = challenge.description
+        res['reward'] = challenge.reward
+        output.append(res)
+
+
+    context =  {"challenges": output, "test":'ok'}
+    return render(request, "main.html", context)
+
+
+
+def create_challenge(request):
+
+    return render(request, "create_challenge.html")
+
+def new_challenge(request):
+    data = request.POST
+    challenge = Challenge.objects.create(title=data['title'],  reward = data['reward'], description = data['description'] )
+    context = {'title': challenge.title, 'reward': challenge.reward, 'date': challenge.date,
+               'description': challenge.description}
+    # return Response(serializer.data)
+    return render(request, "challenge.html", context)
+
+
